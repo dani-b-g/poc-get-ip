c2f7e2482798ee53a8f2bb58f066a36221ba843a
Daniel Barriga Grados <dbarriga@viewnext.com>
2024-05-15 17:37:42 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Cambia esto a la URL de tu frontend en producción
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(443);
 }
 bootstrap();
